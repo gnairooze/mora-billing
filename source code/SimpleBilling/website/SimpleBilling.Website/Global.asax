233a9342c7945b31c84237b23c69fcef8529fe1e
gnairooze <gnairooze@gmail.com>
2016-06-17 20:26:57 +0200

    create bare-bone website - no implementation

---
+++ source code/SimpleBilling/website/SimpleBilling.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleBilling.Website.MvcApplication" Language="C#" %>
---
+++ source code/SimpleBilling/website/SimpleBilling.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleBilling.Website.Startup))]
+namespace SimpleBilling.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
